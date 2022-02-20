4eef8c3680689f58534871d0e48cab9ebd0e5853
Anna Kangasmaa <anna.kangasmaa@helsinki.fi>
2022-02-20 19:51:00 +0200

    add transactions, stats

--- src/schema.sql
+++ src/schema.sql
@@ -8,16 +8,39 @@ CREATE TABLE stocks (
     id SERIAL PRIMARY KEY,
     company TEXT,
     amount INTEGER,
-    buy_date TEXT,
-    sell_date TEXT,
+    buy_date DATE,
+    sell_date DATE,
     buy_price FLOAT,
-    sell_price FLOAT
+    sell_price FLOAT,
+    portfolio_id INTEGER REFERENCES portfolios
 );
 
 CREATE TABLE portfolios (
     id SERIAL PRIMARY KEY,
-    username TEXT,
-    date TEXT,
-    name TEXT,
-    stock_id INTEGER REFERENCES stocks
+    owner TEXT,
+    date DATE,
+    name TEXT
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    date DATE,
+    company TEXT,
+    buy_price FLOAT,
+    sell_price FLOAT,
+    banking FLOAT,
+    dividend FLOAT,
+    portfolio_id INTEGER REFERENCES portfolios
+);
+
+CREATE TABLE stats (
+    id SERIAL PRIMARY KEY,
+    portfolio_name TEXT,
+    purchases FLOAT,
+    banking FLOAT,
+    taxes FLOAT,
+    sales FLOAT,
+    dividends FLOAT,
+    net FLOAT,
+    user_id INTEGER REFERENCES users
 );
