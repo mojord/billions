43aaa67f4c4d602b5cf52854fe4fc2fe3c744f97
Anna Kangasmaa <anna.kangasmaa@helsinki.fi>
2022-02-20 19:49:44 +0200

    add buy, sell, gameover

--- src/repositories/market_repository.py
+++ src/repositories/market_repository.py
@@ -5,19 +5,75 @@ class MarketRepository:
     def __init__(self):
         pass
     
-    def create_portfolio(self, username, date, name):
-        sql = "INSERT INTO portfolios (username, date, name) VALUES (:username, :date, :name)"
-        db.session.execute(sql, {"username":username, "date":date, "name":name})
+    def create_portfolio(self, owner, date, name):
+        sql = "INSERT INTO portfolios (owner, date, name) VALUES (:owner, :date, :name)"
+        db.session.execute(sql, {"owner":owner, "date":date, "name":name})
         db.session.commit()
 
-    def create_stock(self, company, amount, buy_date, sell_date, buy_price, sell_price):
-        sql = """INSERT INTO stocks (company, amount, buy_date, sell_date, buy_price, sell_price)
-            VALUES (:company, :amount, :buy_date, :sell_date, :buy_price, :sell_price)"""
-        db.session.execute(sql, {"company":company, "amount":amount, "buy_date":buy_date, "sell_date":sell_date, "buy_price":buy_price, "sell_price":sell_price})
+    def create_stock(self, company, amount, buy_date, sell_date, buy_price, sell_price, portfolio_id):
+        sql = """INSERT INTO stocks (company, amount, buy_date, sell_date, buy_price, sell_price, portfolio_id)
+            VALUES (:company, :amount, :buy_date, :sell_date, :buy_price, :sell_price, :portfolio_id)"""
+        db.session.execute(sql, {"company":company, "amount":amount, "buy_date":buy_date, "sell_date":sell_date, "buy_price":buy_price, "sell_price":sell_price, "portfolio_id":portfolio_id})
+        db.session.commit()
+    
+    def find_stocks(self, portfolio_id):
+        sql = "SELECT * FROM stocks WHERE portfolio_id=:portfolio_id AND amount IS NOT NULL ORDER BY buy_date"
+        result = db.session.execute(sql, {"portfolio_id":portfolio_id})
+        return result.fetchall()
+    
+    def check_portfolio(self, owner):
+        sql = "SELECT * FROM portfolios WHERE owner=:owner"
+        result = db.session.execute(sql, {"owner":owner}).fetchall()
+        if len(result)==0:
+            return False
+        else:
+            return True
 
-    def find_portfolio_by_username(self, username):
-        sql = "SELECT username FROM portfolio WHERE username=:username"
-        result = db.session.execute(sql, {"username":username})
-        return result.fetchone()
+    def find_portfolio_name(self, owner):
+        sql = "SELECT name FROM portfolios WHERE owner=:owner"
+        result = db.session.execute(sql, {"owner":owner})
+        return result.fetchone()[0]
+
+    def find_portfolio_id_by_owner(self, owner):
+        sql = "SELECT id FROM portfolios WHERE owner=:owner"
+        result = db.session.execute(sql, {"owner":owner})
+        return result.fetchone()[0]
+    
+    def get_sellable_stocks(self, company, portfolio_id):
+        sql = "SELECT * FROM stocks WHERE company=:company AND portfolio_id=:portfolio_id AND amount >= 0 ORDER BY buy_date"
+        result = db.session.execute(sql, {"company":company, "portfolio_id":portfolio_id})
+        return result.fetchall()
+    
+    def sell(self, amount, sell_date, sell_price, id):
+        sql = "UPDATE stocks SET amount=:amount*-1, sell_date=:sell_date, sell_price=:sell_price WHERE id=:id"
+        db.session.execute(sql, {"amount":amount, "sell_date":sell_date, "sell_price":sell_price, "id":id})
+        db.session.commit()
+
+    def buy(self, amount):
+        sql = "UPDATE stocks SET amount=:amount WHERE amount=0"
+        db.session.execute(sql, {"amount":amount})
+        db.session.commit()
+    
+    def delete_sold_stocks(self):
+        sql = "DELETE FROM stocks WHERE amount < 0"
+        db.session.execute(sql)
+        db.session.commit()
+    
+    def add_transaction(self, date, company, buy_price, sell_price, banking, dividend, portfolio_id):
+        sql = """INSERT INTO transactions (date, company, buy_price, sell_price, banking, dividend, portfolio_id)
+            VALUES (:date, :company, :buy_price, :sell_price, :banking, :dividend, :portfolio_id)"""
+        db.session.execute(sql, {"date":date, "company":company, "buy_price":buy_price, "sell_price":sell_price, "banking":banking, "dividend":dividend, "portfolio_id":portfolio_id})
+        db.session.commit()
+    
+    def get_latest_transaction(self, portfolio_id):
+        sql = "SELECT MAX(date) FROM transactions WHERE portfolio_id=:portfolio_id"
+        result = db.session.execute(sql, {"portfolio_id":portfolio_id})
+        db.session.commit()
+        return result.fetchone()[0]
+    
+    def find_transactions(self, portfolio_id):
+        sql = "SELECT * FROM transactions WHERE portfolio_id=:portfolio_id"
+        result = db.session.execute(sql, {"portfolio_id":portfolio_id})
+        return result.fetchall()
 
 market_repository = MarketRepository()
--- src/services/market_service.py
+++ src/services/market_service.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from repositories.market_repository import market_repository as default_market_repository
 
 
@@ -5,11 +6,54 @@ class MarketService:
     def __init__(self, market_repository=default_market_repository):
         self._market_repository = market_repository
 
-    def create_portfolio(self, name, date, owner):
-        self._market_repository.create_portfolio(name, date, owner)
-    
-    def create_stock(self, company, amount, buy_date, sell_date, buy_price, sell_price):
-        self._market_repository.create_stock(company, amount, buy_date, sell_date, buy_price, sell_price)
+    def find_stocks(self, portfolio_id):
+        stocks = self._market_repository.find_stocks(portfolio_id)
+        return stocks
 
+    def create_portfolio(self, owner, date, name):
+        self._market_repository.create_portfolio(owner, date, name)
+
+    def find_portfolio_name(self, owner):
+        portfolio_name = self._market_repository.find_portfolio_name(owner)
+        return portfolio_name
+    
+    def create_stock(self, company, amount, buy_date, sell_date, buy_price, sell_price, portfolio_id):
+        self._market_repository.create_stock(company, amount, buy_date, sell_date, buy_price, sell_price, portfolio_id)
+    
+    def update_amount(self, company, amount, buy_date, sell_date, buy_price, sell_price, portfolio_id):
+        self._market_repository.update_amount(company, amount, buy_date, sell_date, buy_price, sell_price, portfolio_id)
+
+    def find_portfolio_id(self, owner):
+        id = self._market_repository.find_portfolio_id_by_owner(owner)
+        return id
+    
+    def check_portfolio(self, owner):
+        check = self._market_repository.check_portfolio(owner)
+        return check
+    
+    def get_sellable_stocks(self, company, portfolio_id):
+        sellable_stocks = self._market_repository.get_sellable_stocks(company, portfolio_id)
+        return sellable_stocks
+    
+    def sell(self, amount, sell_date, sell_price, id):
+        self._market_repository.sell(amount, sell_date, sell_price, id)
+    
+    def buy(self, amount):
+        self._market_repository.buy(amount)
+    
+    def delete_sold_stocks(self):
+        self._market_repository.delete_sold_stocks()
+    
+    def add_transaction(self, date, company, buy_price, sell_price, banking, dividend, portfolio_id):
+        self._market_repository.add_transaction(date, company, buy_price, sell_price, banking, dividend, portfolio_id)
+    
+    def get_latest_transaction(self, portfolio_id):
+        date = self._market_repository.get_latest_transaction(portfolio_id)
+        return date
+
+    def find_transactions(self, portfolio_id):
+            transactions = self._market_repository.find_transactions(portfolio_id)
+            return transactions
+    
 
 market_service = MarketService()
