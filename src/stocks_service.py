0be483f549569e7b37326f3862ed18e76bf78687
Anna Kangasmaa <anna.kangasmaa@helsinki.fi>
2022-03-01 16:33:11 +0200

    add end value counter

--- src/stocks_service.py
+++ src/stocks_service.py
@@ -23,6 +23,10 @@ class StocksService:
 #                print(key, value)
 #        print(f"tässä päiväys {date}")
                 return label, value[0], value[1], date
+    
+    def last_day(self):
+        last_prices = {"Elisa": 54.177, "Finnair": 0.59, "Fortum": 27.05, "Kone": 63.084, "Metso": 9.368, "Neste": 43.328, "Nokia": 5.582, "Nordea": 10.828, "Orion B": 36.429, "Sampo": 44.147, "UPM": 33.502, "Wärtsilä": 12.366}
+        return last_prices
 
 
 stocks_service = StocksService()
